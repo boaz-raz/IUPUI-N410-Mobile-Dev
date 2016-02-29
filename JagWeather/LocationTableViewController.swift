b8de72da8ec90050caa7ed9c5a218d753c121180
Boaz Raz <raz.boaz@gmail.com>
2016-02-29 14:15:51 -0500

    Finish the delete function

--- JagWeather/LocationTableViewController.swift
+++ JagWeather/LocationTableViewController.swift
@@ -40,38 +40,62 @@ class LocationTableViewController: UITableViewController {
         
         return cell
         
-        
-        
-        
-        // func to confirm delete
-        func confirmDelete(planet: String) {
-            let alert = UIAlertController(title: "Delete Location", message: "Are you sure you want to permanently delete \(WeatherLocation.name)?", preferredStyle: .ActionSheet)
-            
-            let DeleteAction = UIAlertAction(title: "Delete", style: .Destructive, handler: handleDeletePlanet)
-            let CancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: cancelDeletePlanet)
-            
-            alert.addAction(DeleteAction)
-            alert.addAction(CancelAction)
-            
-            
-            // Support display in iPad
-            
-            //alert.popoverPresentationController?.sourceView = self.view
-            //alert.popoverPresentationController?.sourceRect = CGRectMake(self.view.bounds.size.width / 2.0, self.view.bounds.size.height / 2.0, 1.0, 1.0)
-            
-            self.presentViewController(alert, animated: true, completion: nil)
-        } // end confirmDelete fun
-        
-        
-        func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-            if editingStyle == .Delete {
-                deleteLocationIndexPath = indexPath
-                let locationToDelete = weatherLocationStore.allLocations[indexPath.row]
-                //confirmDelete(planetToDelete)
-            }
-        } // ent
+    
         
     } // end of tableView fun
+    
+    
+    
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            deleteLocationIndexPath = indexPath
+            let locationToDelete = weatherLocationStore.allLocations[indexPath.row]
+    
+            confirmDelete(locationToDelete.name)
+        }
+    } // end tableView
+    
+    
+    // func to confirm delete
+    func confirmDelete(planet: String) {
+        let alert = UIAlertController(title: "Delete Location", message: "Are you sure you want to permanently delete \(weatherLocationStore)?", preferredStyle: .ActionSheet)
+        
+        let DeleteAction = UIAlertAction(title: "Delete", style: .Destructive, handler: handleDeleteLocation)
+        let CancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: cancelDeleteLocation)
+        
+        alert.addAction(DeleteAction)
+        alert.addAction(CancelAction)
+        
+        
+        // Support display in iPad
+        
+        //alert.popoverPresentationController?.sourceView = self.view
+        //alert.popoverPresentationController?.sourceRect = CGRectMake(self.view.bounds.size.width / 2.0, self.view.bounds.size.height / 2.0, 1.0, 1.0)
+        
+        self.presentViewController(alert, animated: true, completion: nil)
+    } // end confirmDelete func
+    
+    
+    
+    
+    func handleDeleteLocation(alertAction: UIAlertAction!) -> Void {
+        if let indexPath = deleteLocationIndexPath {
+            tableView.beginUpdates()
+            
+            weatherLocationStore.allLocations.removeAtIndex(indexPath.row)
+            
+            // Note that indexPath is wrapped in an array:  [indexPath]
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+            
+            deleteLocationIndexPath = nil
+            
+            tableView.endUpdates()
+        }
+    }
+    
+    func cancelDeleteLocation(alertAction: UIAlertAction!) {
+        deleteLocationIndexPath = nil
+    }
     
     
     
